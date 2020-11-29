b10ace8ef90a77aa6da064969b651032fad17ae3
Lucas <lemmg@cin.ufpe.br>
2020-11-28 22:41:53 -0300

    Feature ([feature/front-whoWeAre]): creation of the section who we are

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,17 @@
+import './index.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Primeiro from './components/Primeiro'
+import Photo from './components/WhoWeAre'
 import App from './App';
+import WhoWeAre from './components/WhoWeAre';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<div>
+		<WhoWeAre>
+			
+		</WhoWeAre>
+	</div>,
   document.getElementById('root'),
 );
